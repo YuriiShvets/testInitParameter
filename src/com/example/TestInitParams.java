5890a421ca92bde048f884038fde382d3f8f92a9
Yurii Shvets <yuriishvez@gmail.com>
2017-09-02 11:33:41 +0300

    first commit

---
+++ src/com/example/TestInitParams.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Created by Юра on 02.09.2017.
+ */
+@WebServlet(name = "TestInitParams")
+public class TestInitParams extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("test init parameters<br>");
+
+        Enumeration e = getServletConfig().getInitParameterNames();
+        while(e.hasMoreElements()) {
+            out.println("<br>param name - " + e.nextElement() + "<br>");
+        }
+        out.println("main email is " + getServletConfig().getInitParameter("mainEmail") + "<br>");
+        out.println("admin email is " + getServletConfig().getInitParameter("adminEmail"));
+    }
+}
